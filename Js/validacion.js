c36951382553f6b95a2a188aba41e74074944c10
Gabriel Rios <luisgabrielrios@gmail.com>
2023-06-11 01:19:56 -0300

    limpieza css

--- Js/validacion.js
+++ Js/validacion.js
@@ -2,31 +2,34 @@ function validarFormulario(event) {
     event.preventDefault();
 
     let formulario = document.getElementById('miFormulario');
-    let nombre = formulario.elements ['nombre'].value;
-    let telefono = formulario.elements ['telefono'].value;
-    let email = formulario.elements ['email'].value;
-    let sugerencias = formulario.elements ['sugerencias'].value;
-    
+    let nombre = formulario.elements['nombre'].value;
+    let telefono = formulario.elements['telefono'].value;
+    let email = formulario.elements['email'].value;
+    let sugerencias = formulario.elements['sugerencias'].value;
+
     console.log('nombre')
     if (!nombre || !email || !telefono || !sugerencias) {
         alert("Por favor, complete todos los campos.");
     }
+    else {
+        if (nombre.length < 3) {
+            alert("El nombre debe tener mas de 3 caracteres.");
+        }
+        if (sugerencias.length < 10) {
+            alert("Por favro escribenos mas de 10 caracteres.");
+        }
 
-    if (nombre.length < 3 ) {
-        alert("El nombre debe tener mas de 3 caracteres.");
+        // Validar el formato del mail utilizando una expresión regular
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!email.match(emailRegex)) {
+            alert("Por favor, ingrese un correo electrónico válido.");
+        }
+
+        // Validar el formato del número de teléfono utilizando una expresión regular
+        let telefonoRegex = /^[0-9]{10}$/;
+        if (!telefono.match(telefonoRegex)) {
+            alert("Por favor, ingrese un número de teléfono válido de 10 dígitos.");
+        }
     }
 
-    // Validar el formato del mail utilizando una expresión regular
-    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!email.match(emailRegex)) {
-        alert("Por favor, ingrese un correo electrónico válido.");
-    }
-
-    // Validar el formato del número de teléfono utilizando una expresión regular
-    let telefonoRegex = /^[0-9]{10}$/;
-    if (!telefono.match(telefonoRegex)) {
-        alert("Por favor, ingrese un número de teléfono válido de 10 dígitos.");
-    }
-
-    
 }
